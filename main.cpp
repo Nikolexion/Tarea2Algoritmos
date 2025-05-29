f7ff82769c765094b01bc77042e09012d3b2a28e
Nikolexion <nfalc2003@gmail.com>
2025-05-29 12:57:39 -0400

    fix: ahora se reinicia correctamente el map entre llamadas y además se agregó una forma para cambiar entre meoizacion y recursividad

--- main.cpp
+++ main.cpp
@@ -4,12 +4,33 @@
 
 // g++ .\main.cpp .\editDistanceMemo.cpp .\editDistanceRecursive.cpp -o main
 int main(){
-    std::cout << "Distancia gato y perro: " << (editDistanceRecursive("gato","perro",4,5)) << std::endl;
-    std::cout << "Distancia gato y gallina: " << (editDistanceRecursive("gato","gallina",4,7)) << std::endl;;
-    std::cout << "Distancia gato y patito: " << (editDistanceRecursive("gato","patito",4,6)) << std::endl;;
-    std::cout << "Distancia perro y gallina: " << (editDistanceRecursive("perro","gallina",5,7)) << std::endl;;
-    std::cout << "Distancia perro y patito: " << (editDistanceRecursive("perro","patito",5,6)) << std::endl;;
-    std::cout << "Distancia gallina y perro: " << (editDistanceRecursive("gallina","patito",7,6)) << std::endl;;
+    bool usarMemoization = false;
+    if (usarMemoization){
+        std::cout << "Distancia gato y perro: " << (editDistanceRecursive("gato","perro",4,5)) << std::endl;
+        std::cout << "Distancia gato y gallina: " << (editDistanceRecursive("gato","gallina",4,7)) << std::endl;;
+        std::cout << "Distancia gato y patito: " << (editDistanceRecursive("gato","patito",4,6)) << std::endl;;
+        std::cout << "Distancia perro y gallina: " << (editDistanceRecursive("perro","gallina",5,7)) << std::endl;;
+        std::cout << "Distancia perro y patito: " << (editDistanceRecursive("perro","patito",5,6)) << std::endl;;
+        std::cout << "Distancia gallina y perro: " << (editDistanceRecursive("gallina","patito",7,6)) << std::endl;;
+    } else {
+        std::map<std::pair<int, int>, int> memo;
+        std::cout << "Distancia gato y perro: " << (editDistanceMemo("gato","perro",4,5,memo)) << std::endl;
+        memo.clear(); // Limpiar para la siguiente llamada
+
+        std::cout << "Distancia gato y gallina: " << (editDistanceMemo("gato","gallina",4,7,memo)) << std::endl;
+        memo.clear(); 
+
+        std::cout << "Distancia gato y patito: " << (editDistanceMemo("gato","patito",4,6,memo)) << std::endl;
+        memo.clear();
+
+        std::cout << "Distancia perro y gallina: " << (editDistanceMemo("perro","gallina",5,7,memo)) << std::endl;
+        memo.clear(); 
+
+        std::cout << "Distancia perro y patito: " << (editDistanceMemo("perro","patito",5,6,memo)) << std::endl;
+        memo.clear();
+
+        std::cout << "Distancia gallina y patito: " << (editDistanceMemo("gallina","patito",7,6,memo)) << std::endl;
+    }
     return 0;
 }
 
