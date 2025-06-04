22401161ff272364f6e92ec54a09de4a6c77e11c
DyStorzion <benja2705@gmaail.com>
2025-06-04 16:08:10 -0400

    fix: se corrige bug en parametros

--- editDistanceRecursive.h
+++ editDistanceRecursive.h
@@ -2,6 +2,6 @@
 #define EDIT_DISTANCE_RECURSIVE_H
 #include <string>
 
-int editDistanceRecursive(const std::string &str1, std::string &str2,int i, int j);
+int editDistanceRecursive(const std::string &str1, const std::string &str2,int i, int j);
 
 #endif
